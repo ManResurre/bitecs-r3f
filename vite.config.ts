c8a2d68bf0640af3072cf5e4afb0ffc089a7482d
serge <korolariya2@gmail.com>
2025-10-18 11:47:11 +0300

    return plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {TanStackRouterVite} from "@tanstack/router-plugin/vite";
+import {cloudflare} from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [TanStackRouterVite(), react()],
+    plugins: [TanStackRouterVite(), react(), cloudflare()],
     build: {
         outDir: 'dist',
     },
